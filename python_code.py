407cc36d9952eaa944d5cde8614baa2a7dbd9515
Srishti Srivatava <srishtis@skillup.tech>
2019-04-18 12:50:17 +0530

    updated as per week 1 instructions

--- python_code.py
+++ python_code.py
@@ -1,1 +1,5 @@
-print("Hello Github")
+# Coursera Capstone Projecct
+# This notebook is for IBM Data Science Capstone Course
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
